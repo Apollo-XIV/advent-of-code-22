bdd25c2bcb1904cd06114617aaa53296e51d08a4
Apollo-XIV <alexrcrease@gmail.com>
2022-12-07 23:51:55 +0000

    wowza

--- day7.ps1
+++ day7.ps1
@@ -1,80 +1,68 @@
+
+function Path-Gen([string[]]$path) {
+    return (($path -join '/'))
+}
+
+$lines = Get-Content "inputs/day7.txt"
+$dirs = @()
+$dirV = @()
+for ($i=0; $i -lt $lines.Length;$i++) {
+    if ($lines[$i].Substring(0,1) -match '\d') {
+        $ig = 0
+        $path = @()
+        for ($j=$i; $j -ne -1;$j--) {
+            if ($lines[$j].Substring(0, 4) -eq "$ cd") {
+                if ($lines[$j].Substring(5,($lines[$j].Length - 5)) -eq "..") {
+                    $ig += 1
+                } elseif ($ig -ne 0) {
+                    $ig -= 1
+                } else {
+                    $path = @(($lines[$j].Substring(5,($lines[$j].Length - 5)))) + $path
+                }
+            }
+        }
+        for ($k=0; $k -lt $path.Length;$k++) {
+            if (-not $dirs.Contains( $(Path-Gen($path[$k..(- 0)])) )) {
+                $dirs += @($(Path-Gen($path[$k..(- 0)])))
+                $dirV += @(0)
+            }
+
+            $dirV[$dirs.IndexOf($(Path-Gen($path[$k..(- 0)])))] += [Int]($lines[$i] -replace '[^0-9]','')
+        }
+    }
+}
+
+$sum = 0
+for ($i=0; $i -lt $dirs.Length;$i++) {
+    if ($dirV[$i] -le 100000) {
+        $sum += $dirV[$i]
+    }
+}
+Write-Host $sum
+
+$goal = 30000000 - (70000000 - $dirV[0]) #space used
+$dirV = ($dirV | Sort-Object)
+foreach($dir in $dirV) {
+    if ($dir -ge $goal) {
+        Write-Host $dir
+        break
+    }
+}
+
 <#
 
-cd means move pointer
-".." = move up
-else move down
+file path = @("file",parent,parent,parent)
+cd "" add parent
+cd .. remove parent
 
-ls add contents to subarray
+ptsd.nzh -> hsswstq -> ignore .. ----/// bntdqzs //// ---> /
+/hsswstq/ptsd.nzh
 
-class directory {
-    self.contents = @()
-    self.parent = [directory]
-}
+zht -> dmnt -> bsjbvff -> hsswswtq -> /
 
-class file {
-    self.size = [Int]
-    self.parent = [directory]
-}
+directory names arent unique, however, their heritage is
 
-to sum file sizes for each get all directory names and for each sum files that can chase their heritage
+so dmnt/bsjbvff/hsswstq is unique from
 
 
 #>
-
-class dir{
-    [string]$name
-    [Int]$id
-    [dir[]]$contents
-    [string]$parent
-
-    [void] AddChild([dir]$new) {
-        $this.contents += @($new)
-    }
-
-    [Int] findDir($target) {
-        for ($i=0; $i -lt $this.contents.Length; $i++) {
-            if (($this.contents[$i]).id -eq $target) {
-                return $i
-            }
-        }
-        return 0
-    }
-}
-
-class file{
-    [string]$name
-    [Int]$id
-    [string]$size
-    [string]$parent
-}
-
-class pointer{
-    [dir]$wd
-
-    [void] moveup() {
-        $this.wd = $this.wd.parent
-    }
-
-    [void] movedown($target) {
-        foreach($dir in $this.wd.content) {
-            if ($target -eq $dir.name) {
-                $this.wd = $dir
-            }
-        }
-
-        $this.wd.AddChild($target)
-        $this.movedown($target)
-
-    }
-}
-
-$lines = Get-Content "input\day7.txt"
-[dir]$root = [dir]::new()
-[pointer]$ptr = [pointer]::new()
-$ptr.wd = $root
-
-foreach ($line in $lines) {
-    if ($line.Substring(0,4) -eq "$ cd") {
-
-    }
-}
