71f25ae3fea281ffbdb6635f42ae19d6274d22eb
Apollo-XIV <alexrcrease@gmail.com>
2022-12-12 17:55:51 +0000

    day 9 done ;-;

--- day9part2.ps1
+++ day9part2.ps1
@@ -1,4 +1,4 @@
-$moves = Get-Content "inputs/day9test.txt"
+$moves = Get-Content "inputs/day9.txt"
 # tail pointer
 # head pointer
 # tracker = list of tail positions
@@ -7,7 +7,7 @@ $moves = Get-Content "inputs/day9test.txt"
 
 $tracker = @{}
 $rope = @(@(0,0),@(0,0),@(0,0),@(0,0),@(0,0),@(0,0),@(0,0),@(0,0),@(0,0),@(0,0))
-$in = [string]$rope[9][0] + " " + [string]$rope[9][1]
+$in = [string]$rope[$rope.Length-1][0] + " " + [string]$rope[$rope.Length-1][1]
 $tracker.add($in, 0)
 
 foreach ($move in $moves) {
@@ -19,23 +19,44 @@ foreach ($move in $moves) {
             "R" {$rope[0][0] += 1}
         }
 
-        for ($j=1; $j -lt 10; $j++){
-            if (([Math]::Abs($rope[$j-1][0] - $rope[$j][0])) -gt 1) {
-                $rope[$j][0] = $rope[$j][0] + ($rope[$j-1][0] - $rope[$j][0])
+        for ($j=1; $j -lt $rope.Length; $j++){
+            Write-Host "Cur. Knot:" $rope[$j] " Prev. Knot:" $rope[$j-1]
+            <#
+            
+            if a and b
+            if not a
+            if not b
+            else
+
+            alt:
+            - if x.difference -gt 1 move x closer
+            - if y.difference -gt 1 move x closer
+            
+            #>
+            $x = if (([Math]::Abs($rope[$j-1][0] - $rope[$j][0])) -gt 1) {$true} else {$false}
+            $y = if (([Math]::Abs($rope[$j-1][1] - $rope[$j][1])) -gt 1) {$true} else {$false}
+            if ($x -and $y) {
+                $rope[$j][0] = [Int]($rope[$j][0] + $rope[$j-1][0])/2
+                $rope[$j][1] = [Int]($rope[$j][1] + $rope[$j-1][1])/2
+            } elseif ($x) {
+                $rope[$j][0] = [Int]($rope[$j][0] + $rope[$j-1][0])/2
                 $rope[$j][1] = $rope[$j-1][1]
-            }
-            if (([Math]::Abs($rope[$j-1][1] - $rope[$j][1])) -gt 1) {
-                $rope[$j][1] = $rope[$j][1] + ($rope[$j-1][1] - $rope[$j][1])
+            } elseif ($y) {
+                $rope[$j][1] = [Int]($rope[$j][1] + $rope[$j-1][1])/2
                 $rope[$j][0] = $rope[$j-1][0]
             }
+            
         }
-        Write-Host $tracker.Count
-        $in = [string]$rope[9][0] + " " + [string]$rope[9][1]
+
+        $in = [string]$rope[$rope.Length-1][0] + " " + [string]$rope[$rope.Length-1][1]
+        Write-Host "add" $in
         if (-not $tracker.ContainsKey($in)) {$tracker.add($in,0)}
     }
 
 }
+
 Write-Host $rope
+$tracker
 Write-Host $tracker.Count
 <#
 
@@ -70,6 +91,19 @@ alternatively, track wherever the tail goes, add to array, calc final score
 ......
 ..S...
 ......
+
+
+..............
+..012.........
+.....3........
+.....4........
+.....5........
+.....6........
+.....7........
+.....8........
+
+
+
 
 
 
